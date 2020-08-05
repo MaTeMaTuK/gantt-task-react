4dfd3a07ee28974bcf2845690e3c8095712ef79c
unknown <battakycyku@gmail.com>
2020-08-05 08:23:02 +0300

    Updates

---
+++ precache-manifest.2f93820a793c92fd1c7d0406eac1d7a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f45c25eaf5a9be829cb737d8a689135",
+    "url": "./index.html"
+  },
+  {
+    "revision": "556548f5e3f83ba1454c",
+    "url": "./static/css/main.56ef9643.chunk.css"
+  },
+  {
+    "revision": "5095afa3de332659c16e",
+    "url": "./static/js/2.ff483033.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ff483033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556548f5e3f83ba1454c",
+    "url": "./static/js/main.b092aca9.chunk.js"
+  },
+  {
+    "revision": "1caf82591d49edb767c3",
+    "url": "./static/js/runtime-main.e201dd45.js"
+  }
+]);
