dd187eb189b5915794367df5b4bae6c0d693f7da
shaoyh <shaoyh@users.noreply.github.com>
2021-07-21 14:24:47 +0800

    面板调整导致同步滚动异常修复 hover态

--- src/components/other/horizontal-scroll.tsx
+++ src/components/other/horizontal-scroll.tsx
@@ -1,16 +1,24 @@
 import React, { SyntheticEvent, forwardRef } from "react";
 import styles from "./horizontal-scroll.module.css";
 
-export const HorizontalScrollComponent: React.ForwardRefRenderFunction<any, {
-  svgWidth: number;
-  taskListWidth: number;
-  listBottomHeight: number;
-  onScroll: (event: SyntheticEvent<HTMLDivElement>) => void;
-}> = ({ svgWidth, taskListWidth, onScroll, listBottomHeight }, ref) => {
+export const HorizontalScrollComponent: React.ForwardRefRenderFunction<
+  any,
+  {
+    svgWidth: number;
+    taskListWidth: number;
+    listBottomHeight: number;
+    onScroll: (event: SyntheticEvent<HTMLDivElement>) => void;
+  }
+> = ({ svgWidth, taskListWidth, onScroll, listBottomHeight }, ref) => {
+  const dividerWidth = 15;
 
   return (
     <div
-      style={{ marginLeft: taskListWidth, width: `calc(100% - ${taskListWidth}px)`, bottom: `${listBottomHeight}px`}}
+      style={{
+        marginLeft: taskListWidth + dividerWidth,
+        width: `calc(100% - ${taskListWidth}px)`,
+        bottom: `${listBottomHeight}px`,
+      }}
       className={styles.scroll}
       onScroll={onScroll}
       ref={ref}
