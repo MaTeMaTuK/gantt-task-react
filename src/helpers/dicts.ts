763a397287dc2e8cd43fe48bc6009ef7ef057bbb
shaoyh <shaoyh@users.noreply.github.com>
2021-10-28 19:39:57 +0800

    关键路径连线突出展示

--- src/helpers/dicts.ts
+++ src/helpers/dicts.ts
@@ -1,3 +1,7 @@
 export const barBackgroundColorPivotalPath = "#8777D9";
 
 export const barBackgroundColorTimeError = "#FF8F73";
+
+export const errorLinkBorderColor = "#FF8F73";
+
+export const pivotalPathLinkBorderColor = "#6554C0";
