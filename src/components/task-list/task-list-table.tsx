4db481958c7e5d1ba556920794331044acf7fb5f
ody4 <ulysseadimi4@gmail.com>
2022-07-13 02:18:21 +0200

    modify package

--- src/components/task-list/task-list-table.tsx
+++ src/components/task-list/task-list-table.tsx
@@ -105,7 +105,7 @@ export const TaskListTableDefault: React.FC<{
                 maxWidth: rowWidth,
               }}
             >
-              &nbsp;{toLocaleDateString(t.end, dateTimeOptions)}
+              &nbsp;{"toLocaleDateString(t.end, dateTimeOptions)"}
             </div>
             <div
               className={styles.taskListCell}
@@ -114,7 +114,7 @@ export const TaskListTableDefault: React.FC<{
                 maxWidth: rowWidth,
               }}
             >
-              &nbsp;assignedTo
+              &nbsp; {`assignedTo: ${t.assignedUser}`}
             </div>
             
           </div>
