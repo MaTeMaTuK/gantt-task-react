8808069495d3643d0c7c043d0aa46272a23f7b82
Ayush8923 <ayuuniyal7@gmail.com>
2022-08-09 12:02:58 +0530

    tooltip update

--- src/components/other/tooltip.tsx
+++ src/components/other/tooltip.tsx
@@ -125,11 +125,7 @@ export const StandardTooltipContent: React.FC<{
     <div className={styles.tooltipDefaultContainer} style={style}>
       <b style={{ fontSize: fontSize + 6 }}>{`${
         task.name
-      }: ${task.start.getDate()}-${
-        task.start.getMonth() + 1
-      }-${task.start.getFullYear()} - ${task.end.getDate()}-${
-        task.end.getMonth() + 1
-      }-${task.end.getFullYear()}`}</b>
+      }`}</b>
       {task.end.getTime() - task.start.getTime() !== 0 && (
         <p className={styles.tooltipDefaultContainerParagraph}>{`Duration: ${~~(
           (task.end.getTime() - task.start.getTime()) /
