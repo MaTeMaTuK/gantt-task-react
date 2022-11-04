eaa32d55e29bd7530ec78946abf1042c62c79983
Aquiles Oliveira de Souza <aquilesoliveira@MacBook-Air-de-Aquiles-2.local>
2022-11-04 09:41:44 -0300

    Return the title in the tooltip

--- src/components/other/tooltip.tsx
+++ src/components/other/tooltip.tsx
@@ -166,7 +166,7 @@ export const StandardTooltipContent: React.FC<TooltipContentProps> = ({
 
   return (
     <div className={styles.tooltipDefaultContainer} style={style}>
-      <b style={{ fontSize: fontSize + 6 }}>{formaDate(task.start)} - {formaDate(task.end)}</b>
+      <b style={{ fontSize: fontSize + 6 }}>{task.name}: {formaDate(task.start)} - {formaDate(task.end)}</b>
       {task.end.getTime() - task.start.getTime() !== 0 && (
         <p className={styles.tooltipDefaultContainerParagraph}>{`${duration}: ${(
           (task.end.getTime() - task.start.getTime()) /
