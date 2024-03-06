8bf76e1e29a7d3db2b19c8e28a93de4bdd21bd31
anikaislam16 <islamanika252@gmail.com>
2024-03-06 12:33:17 +0600

    Fole

--- src/components/gantt/gantt.tsx
+++ src/components/gantt/gantt.tsx
@@ -257,6 +257,7 @@ export const Gantt: React.FunctionComponent<GanttProps> = ({
   // scroll events
   useEffect(() => {
     const handleWheel = (event: WheelEvent) => {
+      console.log("ul");
       if (event.shiftKey || event.deltaX) {
         const scrollMove = event.deltaX ? event.deltaX : event.deltaY;
         let newScrollX = scrollX + scrollMove;
